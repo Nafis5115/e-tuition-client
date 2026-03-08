c90c03776ccd979e27996a3f386e60d17525ca62
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-03-08 11:40:11 +0600

    fixed eslint issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
+
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
 
@@ -8,11 +8,7 @@ export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{js,jsx}"],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [js.configs.recommended, reactRefresh.configs.vite],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
